cdb7f3e2a8eae39f651074f9e48e44ce35c2e1d9
Heverton <heverton.gomes2018@gmail.com>
2020-08-26 23:31:10 -0300

    Criação primeira versão da modelagem

--- inventory/items/models.py
+++ inventory/items/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from inventory.core.models import Base
+from django.utils.translation import ugettext_lazy as _
+.
 
-# Create your models here.
+UNIT_CHOICES = [
+    ("0", "UND"),
+    ("1", "G"),
+    ("2", "MG"),
+    ("3", "L"),
+    ("4", "ML"),
+    ("5", "KL")
+]
+
+
+class Item(Base):
+
+    name = models.CharField(_("Nome do Item"), max_length=100)
+    unit = models.CharField(_("Unidade"), max_length=1, choices=UNIT_CHOICES)
+    brand = models.CharField(_("Marca"), max_length=100)
+    current_inventory = models.DecimalField(_("Estoque atual"), max_digits=5)
+    minimum_stock = models.DecimalField(_("Estoque minímo"), max_digits=5)
+    validaty = models.DateField(_("Data de Validade"), auto_now=False, auto_now_add=False)
+
+    class meta:
+        db_table = 'tb_item'
+    
+    def __str__(self):
+        return self.name
+    
--- inventory/stock/models.py
+++ inventory/stock/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from inventory.core.models import Base
+from inventory.items.models import Item
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+MOVEMENT = [
+    ("e", "Entrada")
+    ("s", "Saída")
+]
+
+class Stock(Base):
+    nf = models.PositiveIntegerField('nota fiscal', null=True, blank=True)
+    movement = models.CharField(max_length=1, choices=MOVEMENT)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return str(self.pk)
+
+
+class StockItens(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    qtd = models.PositiveIntegerField()
+    balance = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ('pk',)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.pk, self.stock.pk, self.item)
