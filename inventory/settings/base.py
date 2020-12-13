fd56ec214613ffb8db23dded1dcea0bb133b1cd1
Heverton <heverton.gomes2018@gmail.com>
2020-12-13 13:52:33 -0300

    Correção

--- inventory/settings/base.py
+++ inventory/settings/base.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8080','http://127.0.0.1:8000', 'http://localhost:8000', 'https://inventario-lab-leite.herokuapp.com/'
+    'http://localhost:8080','http://127.0.0.1:8000', 'http://localhost:8000', 'https://inventario-lab-leite.herokuapp.com'
 ]
 
 
